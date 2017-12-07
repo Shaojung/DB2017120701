98787a5838adda9bdb8c1ea3d7df860d32ca6a10
Shaojung <shaojung@gmail.com>
2017-12-07 20:00:58 +0800

    UTF8String Request

--- app/src/main/java/com/example/student/db2017120701/MainActivity.java
+++ app/src/main/java/com/example/student/db2017120701/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-        StringRequest request = new StringRequest("https://udn.com/rssfeed/news/2/6638?ch=news",
+        StringRequest request = new UTF8StringRequest("https://udn.com/rssfeed/news/2/6638?ch=news",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
