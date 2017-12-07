3bf494217bbb97669f4ce0af260a7048ab432b50
Shaojung <shaojung@gmail.com>
2017-12-07 20:43:13 +0800

    UTF8String Request

--- app/src/main/java/com/example/student/db2017120701/MyHandler.java
+++ app/src/main/java/com/example/student/db2017120701/MyHandler.java
@@ -1,5 +1,9 @@
 package com.example.student.db2017120701;
 
+import android.util.Log;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 /**
@@ -7,4 +11,31 @@ import org.xml.sax.helpers.DefaultHandler;
  */
 
 public class MyHandler extends DefaultHandler {
+    boolean isTitle = false;
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        super.startElement(uri, localName, qName, attributes);
+        if (qName.equals("title"))
+        {
+            isTitle = true;
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        super.endElement(uri, localName, qName);
+        if (qName.equals("title"))
+        {
+            isTitle = false;
+        }
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        super.characters(ch, start, length);
+        if (isTitle)
+        {
+            Log.d("NET", new String(ch, start, length));
+        }
+    }
 }
